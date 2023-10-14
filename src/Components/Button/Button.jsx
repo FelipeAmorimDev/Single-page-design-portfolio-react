f1ad65633c413b1b23821bba5af33d1fa8dbbf0e
Felipe Amorim <felipeamorim.go@gmail.com>
2023-10-14 16:24:04 -0300

    Commit inicial

---
+++ src/Components/Button/Button.jsx
@@ -0,0 +1,9 @@
+import style from './Button.module.scss'
+
+const Button = ({value, classN}) => {
+  return (
+    <button className={style[classN]}>{value}</button>
+  )
+}
+
+export default Button
